c2f084cb14fe8f2a775367e759f2920670f0758b
123Ibragim <120119260+123Ibragim@users.noreply.github.com>
2022-12-13 17:02:34 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,29 @@
+// let list = "putin biden shavkat zelenskiy"
+// let president = prompt("What is your name?")
+// if(alert >= "zelenskiy" === "putin"){
+//     alert("welcome to war");
+// } else{
+//     alert("nikakoy voyni");
+// }
+
+// // var name = prompt ("NAME,CENAME?",);
+// //          var age = prompt('age?',18);
+         
+// //          alert ('Hello' + name);
+// //          var message = (age === 18) ? 'vi minor':
+// //          alert(message);
+
+// //          if(age >= 18 )
+
+
+var a=prompt("Укажите своё имя:"),
+
+     b=prompt("Укажите свою фамилию:"),
+
+     c=prompt("Укажите свой возраст:");
+
+alert("Здравствуйте "+a+" "+b);
+
+alert(c<18?"Вы не совершеннолетний":"Вы совершеннолетний");
+
+console.log(a, b, c,);
